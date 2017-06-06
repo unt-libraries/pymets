fc3824dc5d7b19a9cc937f950643bdfc7d5d5aed
Lauren Ko <lauren.ko@unt.edu>
2017-06-06 17:38:24 -0500

    Add setup.

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='pymets',
+    version='1.0.0',
+    description='Python module for reading and writing METS files.',
+    author='University of North Texas Libraries',
+    author_email='mark.phillips@unt.edu',
+    url='https://github.com/unt-libraries/pymets',
+    license='BSD',
+    packages=['pymets'],
+    install_requires=[
+        'lxml>=3.4.4',
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ]
+)
